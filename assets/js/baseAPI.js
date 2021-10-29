7fe46fb68e1071aaf8240f3eee77b2efe03cf15f
zzw <929941178@qq.com>
2021-10-30 06:13:55 +0800

    给本地存储添加了一个有效时间

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,11 +1,26 @@
 //ajaxPrefilter会在真正执行Ajax前调用ajaxPrefilter中的配置
 $.ajaxPrefilter(function(options){
+    
+    //重写localStorage的getExpire方法
+    Storage.prototype.getExpire = key => {
+        let val = localStorage.getItem(key);
+        if (!val) {
+            return val;
+        }
+        val = JSON.parse(val);
+        if (Date.now() - val.time > val.expire) {
+            localStorage.removeItem(key);
+            return null;
+        }
+        return val.data;
+    }
+
     //拼接接口地址，使接口完整
     options.url='http://api-breakingnews-web.itheima.net'+options.url;
     //判断接口地址中是否包含my，是的话添加headers请求头
     if(options.url.indexOf('/my/')!==-1){
         options.headers={
-            Authorization:localStorage.getItem('token') || ''
+            Authorization:localStorage.getExpire('token') || ''
         };
     }
     //调用complete回调函数，判断请求失败的话直接退回到登录界面
@@ -13,7 +28,7 @@ $.ajaxPrefilter(function(options){
         //判断res结果是否请求失败
         if(res.responseJSON.status===1 && res.responseJSON.message==='身份认证失败！'){
             //强制清空本地存储token
-            localStorage.removeItem('token');
+            sessionStorage.removeItem('token');
             //跳转到登录页面
             location.href='/login.html';
         }
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,40 +1,40 @@
-$(function(){
+$(function () {
     //点击去注册标签事件
-    $("#link_login").on('click',function(){
+    $("#link_login").on('click', function () {
         $(".reg-box").show();
         $(".login-box").hide();
     })
     //点击去登录标签事件
-    $("#link_reg").on('click',function(){
+    $("#link_reg").on('click', function () {
         $(".reg-box").hide();
         $(".login-box").show();
     })
 
     //获取layui的form对象
-    var form=layui.form;
+    var form = layui.form;
     //获取layui的layer对象
-    var layer=layui.layer;
+    var layer = layui.layer;
     form.verify({
         //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
         pass: [
             /^[\S]{6,12}$/
-            ,'密码必须6到12位，且不能出现空格'
-        ] ,
-        repass:function(value){
-            var repass=$(".reg-box [name=password]").val();
-            if(repass!==value){
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        repass: function (value) {
+            var repass = $(".reg-box [name=password]").val();
+            if (repass !== value) {
                 return '再次确认密码必须和密码一致';
             }
         }
-    }); 
+    });
 
     //注册表单的提交事件
-    $("#form_reg").on('submit',function(e){
+    $("#form_reg").on('submit', function (e) {
         //阻止提交按钮的默认行为
         e.preventDefault();
-        var data={username:$("#form_reg [name=username]").val(),password:$("#form_reg [name=password]").val()}
-        $.post('/api/reguser',data,function(res){
-            if(res.status!=0){
+        var data = { username: $("#form_reg [name=username]").val(), password: $("#form_reg [name=password]").val() }
+        $.post('/api/reguser', data, function (res) {
+            if (res.status != 0) {
                 return layer.msg(res.message);
             }
             layer.msg(res.message);
@@ -43,23 +43,34 @@ $(function(){
     })
 
     //登录表单的提交事件
-    $("#form_login").on('submit',function(e){
+    $("#form_login").on('submit', function (e) {
         //阻止提交默认行为
         e.preventDefault();
         $.ajax({
-            url:'/api/login',
-            method:'POST',
+            url: '/api/login',
+            method: 'POST',
             //获取表单中所有元素的内容
-            data:$(this).serialize(),
-            success:function(res){
-                if(res.status!=0){
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status != 0) {
                     return layer.msg(res.message);
                 }
                 layer.msg(res.message);
+                //重写localStorage的setExpire方法
+                Storage.prototype.setExpire = (key, value, expire) => {
+                    let obj = {
+                        data: value,
+                        time: Date.now(),
+                        expire: expire
+                    };
+                    //localStorage 设置的值不能为对象,转为json字符串
+                    localStorage.setItem(key, JSON.stringify(obj));
+                }
+
                 //将返回的token存入本地存储中
-                localStorage.setItem('token',res.token)
+                localStorage.setExpire('token', res.token, 86400000)
                 //跳转到后台主页
-                location.href='/index.html';
+                location.href = '/index.html';
             }
         })
     })
